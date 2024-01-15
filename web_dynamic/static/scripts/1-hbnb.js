7c298647f55784f572654f43fac70ff2dcab88c4
Benie2002 <isharabenie@gmail.com>
2024-01-15 17:53:07 +0000

    files

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+
+    updateAmenities();
+  });
+
+  function updateAmenities () {
+    const amenityNames = Object.values(checkedAmenities);
+    $('.amenities h4').text(amenityNames.join(', '));
+  }
+});
